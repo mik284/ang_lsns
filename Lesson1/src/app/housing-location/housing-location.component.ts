b8a018cb993de15848d1b29340ec1a4bc4e5363a
mik284 <mikeykamau222@gmail.com>
2024-02-08 18:51:31 +0300

    updated home and housing & location

--- Lesson1/src/app/housing-location/housing-location.component.ts
+++ Lesson1/src/app/housing-location/housing-location.component.ts
@@ -1,13 +1,23 @@
-import { Component, Input} from '@angular/core';
+import { Component, EventEmitter, Input, Output} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housingLocation';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   template: `
     <section class="listing">
+      <form>
+        <input
+        name="text"
+          ngModel=""
+          (ngModelChange)="(handleFormChange($event))"
+          type="text"
+          placeholder="Filter by city"
+        />
+      </form>
       <img
         class="listing-photo"
         [src]="housingLocation.photo"
@@ -23,4 +33,9 @@ import { HousingLocation } from '../housingLocation';
 })
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
+  @Output() cityTitle = new EventEmitter();
+
+  handleFormChange(e:any) {
+    this.cityTitle.emit(e)
+  }
 }
