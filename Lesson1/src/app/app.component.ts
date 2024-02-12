bc070e15087de05a4b057268b9ef2a800205a896
mik284 <mikeykamau222@gmail.com>
2024-02-12 08:32:55 +0300

    add outlet to app component

--- Lesson1/src/app/app.component.ts
+++ Lesson1/src/app/app.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
   template: ` <main>
-    <header class="brand-name">
-      <img
-        class="brand-logo"
-        src="/assets/logo.svg"
-        alt="logo"
-        aria-hidden="true"
-      />
-    </header>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+    </a>
     <section class="content">
-      <app-home></app-home>
+      <!-- <app-home></app-home> -->
+      <!-- The routed views render in the <router-outlet>-->
+      <router-outlet></router-outlet>
     </section>
   </main>`,
   styleUrls: ['./app.component.css'],
