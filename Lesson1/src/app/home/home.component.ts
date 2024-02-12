8f863f4b998faa1656f8b7bb44ed3f69f3b6071b
mik284 <mikeykamau222@gmail.com>
2024-02-12 11:59:46 +0300

    ✨ add json data to be served to service

--- Lesson1/src/app/home/home.component.ts
+++ Lesson1/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 // import { HousingLocation } from '../housingLocation';
@@ -12,35 +12,54 @@ import { FormsModule } from '@angular/forms';
   imports: [CommonModule, HousingLocationComponent, FormsModule],
   template: `
     <section>
-      {{cityTitle}}
+      {{ cityTitle }}
       <button (click)="clickFn('hello')">Click me</button>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter/>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
     <section class="results">
       <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of filteredLocationList"
         [housingLocation]="housingLocation"
-        (cityTitle)="cityTitle=$event"
+        (cityTitle)="cityTitle = $event"
       ></app-housing-location>
     </section>
   `,
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
-  housingLocationList: HousingLocation[] = [];
-  cityTitle ="Kiambu" ;
+export class HomeComponent implements OnInit{
 
-  
+  housingLocationList: HousingLocation[] = [];
+  cityTitle = 'Kiambu';  
+
+  // filteredLocationList holds the values that match the search criteria entered by the user.
+  filteredLocationList: HousingLocation[] = [];
 
   constructor(housingService: HousingService) {
     this.housingLocationList = housingService.getAllHousingLocations();
+    this.filteredLocationList = this.filteredLocationList;
+  }
+  ngOnInit(): void {
+      this.filteredLocationList = this.housingLocationList;
   }
 
   clickFn(message: string): void {
     console.log('message', message);
     alert('I have been clicked');
   }
+
+  filterResults(text: string){
+    if(!text){
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    )
+  }
+
+
 }
