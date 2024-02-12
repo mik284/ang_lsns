bfe24020e052de01cd7c7dfba69c762ad286823f
mik284 <mikeykamau222@gmail.com>
2024-02-12 16:29:29 +0300

    updated  app to use platformbrowserdynamic

--- Lesson1/src/main.ts
+++ Lesson1/src/main.ts
@@ -1,15 +1,22 @@
-/*
- *  Protractor support is deprecated in Angular.
- *  Protractor is used in this example for compatibility with Angular documentation tools.
- */
-import {
-  bootstrapApplication,
-  provideProtractorTestingSupport,
-} from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import routeConfig from './app/routes';
+// /*
+//  *  Protractor support is deprecated in Angular.
+//  *  Protractor is used in this example for compatibility with Angular documentation tools.
+//  */
+// import {
+//   bootstrapApplication,
+//   provideProtractorTestingSupport,
+// } from '@angular/platform-browser';
+// import { AppComponent } from './app/app.component';
+// import { provideRouter } from '@angular/router';
+// import routeConfig from './app/routes';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)],
-}).catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, {
+//   providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)],
+// }).catch((err) => console.error(err));
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
