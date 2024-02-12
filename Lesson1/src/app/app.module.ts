cde1b816c25a2658ab462bf3d05a6b4d2346f1ee
mik284 <mikeykamau222@gmail.com>
2024-02-12 16:29:58 +0300

    used app module *ngmodule directive

---
+++ Lesson1/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import routeConfig from './routes';
+import { HttpClientModule } from '@angular/common/http';
+import { RoutingModule } from './routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+@NgModule({
+  declarations: [
+    HousingLocationComponent,
+    HomeComponent,
+    AppComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    BrowserModule,
+    RoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
+  bootstrap:[AppComponent]
+})
+export class AppModule {}
