a8034efd4c2f06387ff6a70f2dffb35d4269ad4f
mik284 <mikeykamau222@gmail.com>
2024-02-15 12:29:52 +0300

    created a reusable herodetailcomponent

--- tour-of-heroes/src/app/app.module.ts
+++ tour-of-heroes/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgFor
   ],
   providers: [],
   bootstrap: [AppComponent]
